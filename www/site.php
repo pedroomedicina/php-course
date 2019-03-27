11f775cfadf43db397ebcd70f1686b265b0c750d
Pedro Medina <Pedro Medina>
2019-03-27 15:04:43 -0300

    use case of switch statements

--- www/site.php
+++ www/site.php
@@ -9,32 +9,32 @@
     <script src="main.js"></script> -->
 </head>
 <body>
-
-        <?php
-          function getMax($num1, $num2){
-            if($num1 > $num2){
-              return $num1;
-            } else {
-              return $num2;
-            }
-          };
-
-          echo getMax(300, 90);
-          echo "<br>";
-
-          //more complex version
-
-          function getMaxT($num1, $num2, $num3){
-            if($num1 >= $num2 && $num1 >= $num3){
-              return $num1;
-            } elseif($num2 >= $num1 && $num2 >= $num3){
-              return $num2;
-            }else {
-              return $num3;
-            }
-          };
-
-          echo getMaxT(300, 90, 400);
-        ?>
+  <form action="site.php" method="post">
+    What was your grade?<br>
+    <input type="text" name="grade">
+    <input type='submit'>
+  </form>
+  <?php
+    $grade = $_POST['grade'];
+    switch($grade){
+      case "A":
+      echo "You did amazing!";
+      break;
+      case "B":
+      echo "You did pretty good";
+      break;
+      case "C":
+      echo "You did poorly";
+      break;
+      case "D":
+      echo "You did very bad";
+      break;
+      case "F":
+      echo "YOU FAIL!";
+      break;
+      default:
+      echo "Invalid Grade";
+    }
+  ?>
 </body>
 </html>
