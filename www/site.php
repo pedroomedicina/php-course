d6e46c165487ef42804da926fc7f25627690fda4
Pedro Medina <Pedro Medina>
2019-03-27 01:20:31 -0300

    How checkboxes can work with php Post method.

--- www/site.php
+++ www/site.php
@@ -10,12 +10,15 @@
 </head>
 <body>
         <form action="site.php" method="post">
-          Password: <input type="text" name="password"> <br>
+          Apples: <input type="checkbox" name="fruits[]" value="apples"><br>
+          Oranges: <input type="checkbox" name="fruits[]" value="oranges"><br>
+          Pears: <input type="checkbox" name="fruits[]" value="pears"><br>
           <input type="submit">
         </form>
 
         <?php
-        echo $_POST["password"];
+          $fruits = $_POST["fruits"];
+          echo $fruits[1];
         ?>
 
 </body>
