5e789da2cf9e228b06a88e87dae3d2c31f11323e
Pedro Medina <Pedro Medina>
2019-03-26 23:20:10 -0300

    getting some input and printing with $_GET[inputname]

--- www/site.php
+++ www/site.php
@@ -9,61 +9,16 @@
     <script src="main.js"></script> -->
 </head>
 <body>
-    
-    <?php
-    echo 40; //positive int
-    echo '<br>';
-    echo -40; //negative int
-    echo '<br>';
-    echo -40.864; //negative floating point number
-    echo '<br>';
-    //perform arithmetics
-    echo 9 + 5;
-    echo '<br>';
-    echo 9 - 5;
-    echo '<br>';
-    echo 9 / 5;
-    echo '<br>';
-    echo 9 * 5.7;
-    echo '<br>';
-    echo (4 + 5) * 10;
-    echo '<br>';
-    //even modulus
-    echo 10 % 3; //getting the remainder printed
-    echo '<br>';
-    
-    $num = 10;
-    echo $num;
-    echo '<br>';
-    $numm++;
-    echo $num;
-    echo '<br>';
-    $numm--;
-    echo $num;
-    echo '<br>';
-    $num += 25;
-    $num *= 20;
-    $num -= 120;
-    echo $num;
-    echo '<br>';
-
-    echo abs(-400); //absolute value
-    echo '<br>';
-    echo pow(2, 4); //power
-    echo '<br>';
-    echo sqrt(144); //square root
-    echo '<br>';
-    echo max(2, 10); //max between 2 num
-    echo '<br>';
-    echo min(2, 10); //min between 2 num
-    echo '<br>';
-    echo round (2.3);
-    echo '<br>';;
-    echo ceil (3.3); //round up
-    echo '<br>';;
-    echo floor (3.3); //round down
-    //dozens and dozens of math functions
-    ?>
+        <form action="site.php" method="get">
+            Name: <input type="text" name="username">
+            <br>
+            Age: <input type="number" name="age">
+            <input type="submit">
+        </form>
+        <br>
+        Your name is <?php echo $_GET["username"]?>
+        <br>
+        Your age is <?php echo $_GET["age"]?>
 
 </body>
 </html>
