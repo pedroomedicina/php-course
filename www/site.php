3c88e5106312de8fbd8f4e20123ca54439a7d07d
Pedro Medina <Pedro Medina>
2019-03-26 22:53:29 -0300

    nums and math functions

--- www/site.php
+++ www/site.php
@@ -11,12 +11,62 @@
 <body>
     
     <?php
+    echo 40; //positive int
+    echo <br>;
+    echo -40; //negative int
+    echo <br>;
+    echo -40.864; //negative floating point number
+    echo <br>;
+    //perform arithmetics
+    echo 9 + 5;
+    echo <br>;
+    echo 9 - 5;
+    echo <br>;
+    echo 9 / 5;
+    echo <br>;
+    echo 9 * 5.7;
+    echo <br>;
+    echo (4 + 5) * 10;
+    echo <br>;
+    //even modulus
+    echo 10 % 3; //getting the remainder printed
+    echo <br>;
+    
+    $num = 10;
+    echo <br>;
+    echo $num;
+    echo <br>;
+    $numm++;
+    echo <br>;
+    echo $num;
+    echo <br>;
+    $numm--;
+    echo <br>;
+    echo $num;
+    echo <br>;
+    $num += 25;
+    echo <br>;
+    $num *= 20;
+    echo <br>;
+    $num -= 120;
+    echo <br>;
 
-        $phrase = "To be or not to be"; //string
-        $age = 30; //integer
-        $gpa = 30.3; //float
-        $isMale = false; //boolean
-        echo $phrase;
+    echo abs(-400); //absolute value
+    echo <br>;
+    echo pow(2, 4); //power
+    echo <br>;
+    echo sqrt(144); //square root
+    echo <br>;
+    echo max(2, 10); //max between 2 num
+    echo <br>;
+    echo min(2, 10); //min between 2 num
+    echo <br>;
+    echo round (2.3);
+    echo <br>;
+    echo ceil (3.3); //round up
+    echo <br>;
+    echo floor (3.3); //round down
+    //dozens and dozens of math functions
     ?>
 
 </body>
