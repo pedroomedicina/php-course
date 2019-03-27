a8045bfd3498cf0c1ada1e05764c681ead84938a
Pedro Medina <Pedro Medina>
2019-03-27 14:40:08 -0300

    if statements within functions

--- www/site.php
+++ www/site.php
@@ -9,19 +9,32 @@
     <script src="main.js"></script> -->
 </head>
 <body>
-     <!--   <form action="site.php" method="post">
-          <input type="text" name="student">
-          <input type="submit">
-        </form> -->
 
         <?php
-          function sayHi($name, $age){
-            echo "Hello $name, you are $age years old. <br>";
-          }
+          function getMax($num1, $num2){
+            if($num1 > $num2){
+              return $num1;
+            } else {
+              return $num2;
+            }
+          };
 
-          sayHi("Tom",20);
-          sayHi("Dave",13);
-          sayHi("Oscar",55);
+          echo getMax(300, 90);
+          echo "<br>";
+
+          //more complex version
+
+          function getMaxT($num1, $num2, $num3){
+            if($num1 >= $num2 && $num1 >= $num3){
+              return $num1;
+            } elseif($num2 >= $num1 && $num2 >= $num3){
+              return $num2;
+            }else {
+              return $num3;
+            }
+          };
+
+          echo getMaxT(300, 90, 400);
         ?>
 </body>
 </html>
